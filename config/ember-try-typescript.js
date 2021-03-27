85f7f3a508427b67e164d1fd4ea14c3ea36c7f49
Chris Krycho <hello@chriskrycho.com>
2021-03-27 16:27:02 -0600

    Type check the app and tests as well as type tests in CI

--- config/ember-try-typescript.js
+++ config/ember-try-typescript.js
@@ -2,21 +2,21 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      command: "tsc --noEmit --project type-tests/base.json",
+      command: "tsc --noEmit && tsc --noEmit --project type-tests/base.json",
       name: "ts-4.1",
       npm: {
         typescript: "~4.1",
       },
     },
     {
-      command: "tsc --noEmit --project type-tests/base.json",
+      command: "tsc --noEmit && tsc --noEmit --project type-tests/base.json",
       name: "ts-4.2",
       npm: {
         typescript: "~4.2",
       },
     },
     {
-      command: "tsc --noEmit --project type-tests/base.json",
+      command: "tsc --noEmit && tsc --noEmit --project type-tests/base.json",
       name: "ts-next",
       allowedToFail: true,
       npm: {

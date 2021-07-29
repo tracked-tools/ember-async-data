fd5c00395602e359598a9ce43fc56ff4e497aca7
Nathaniel Furniss <nlfurniss@gmail.com>
2021-07-29 15:03:32 -0700

    Upgrade ember-qunit to fix the build

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -3,9 +3,10 @@ import config from "dummy/config/environment";
 import { setApplication } from "@ember/test-helpers";
 import { start } from "ember-qunit";
 import QUnit from "qunit";
-
+import { setup } from "qunit-dom";
 import "qunit-dom";
 
+setup(QUnit.assert);
 setApplication(Application.create(config.APP));
 QUnit.config.testTimeout = 1000;
 start();

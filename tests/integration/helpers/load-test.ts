ae26d9ff16a5c3d557b80de1675ca6c34800ca28
Chris Krycho <hello@chriskrycho.com>
2022-02-24 12:41:42 -0700

    Update to Ember v4 types

--- tests/integration/helpers/load-test.ts
+++ tests/integration/helpers/load-test.ts
@@ -1,7 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render, waitFor } from "@ember/test-helpers";
-import type { TestContext } from "ember-test-helpers";
+import { render, waitFor, type TestContext } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 import { defer } from "../../defer";
 

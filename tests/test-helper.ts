d331917478025c91c53341defa7a4a9d77ec561c
Sergey Astapov <SergeAstapov@gmail.com>
2022-10-26 22:36:23 -0400

    run `npx ember-cli-update --to=4.8.0` to align with the latest blueprint

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,12 +1,12 @@
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-import QUnit from 'qunit';
 import { setup } from 'qunit-dom';
-import 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-setApplication(Application.create(config.APP));
 QUnit.config.testTimeout = 1000;
 start();
